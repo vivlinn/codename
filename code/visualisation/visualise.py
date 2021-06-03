ce92272880436d300478eafb05b1066249520229
L0tte <lotte@LAPTOP-J2UJM5ME.localdomain>
2021-06-03 09:36:34 +0200

    add grid to plot

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -5,6 +5,7 @@ def visualise(grid):
     width = grid.get_width()
     height = grid.get_height()
     plt.plot([0, width], [0, height])
+    plt.grid(b=True, which='major', color='#666666', linestyle='-')
 
     for battery in grid.batteries:
         plt.plot(battery.position_x, battery.position_y)
