428b4378de072f25bdffc9cb4c4ca4d0ebfbeab6
VincentEngelhard <engelhardvincent@gmail.com>
2021-06-03 11:35:49 +0200

    Uses command-line argument and added title to plot

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -2,9 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def visualise(grid):
+def visualise(grid, number):
     plt.grid(b=True, which='major', color='#666666', linestyle='-')
+
     
+
+
+
     # https://stackoverflow.com/questions/24943991/change-grid-interval-and-specify-tick-labels-in-matplotlib
     fig, ax = plt.subplots()
     major_ticks = np.arange(0, 51, 10)
@@ -18,18 +22,22 @@ def visualise(grid):
     # Or if you want different settings for the grids:
     ax.grid(which='minor', alpha=0.2)
     ax.grid(which='major', alpha=0.5)
+    # image = np.loadtxt(f'./doc/house.png', delimiter=',')
 
+    ax.set_title(f"District {number}")
+    
 
     for battery in grid.batteries:
-        plt.plot(grid.batteries[battery].position_x, grid.batteries[battery].position_y, "r+")
+        plt.plot(grid.batteries[battery].position_x, grid.batteries[battery].position_y, "rX")
 
     for house in grid.houses:
-        plt.plot(grid.houses[house].position_x, grid.houses[house].position_y, "bo")
+        plt.plot(grid.houses[house].position_x, grid.houses[house].position_y, "bx")
 
     # for cable in grid.cables:
     #     plt.plot(cable.start, cable.end)
 
-    plt.savefig("grid.png")
+    # saves number of district in name
+    plt.savefig(f"doc/district_{number}.png")
     plt.show()
 
 
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-
+from sys import argv
 from code.classes import grid
 from code.visualisation import visualise
+from os import path
 
 
 if __name__ == "__main__":
-    file_batteries = f"data/district_1/district-1_batteries.csv"
-    file_houses = f"data/district_1/district-1_houses.csv"
+    
+    # check if command-line contains two arguments
+    if len(argv) != 2:
+        print("Usage: python3 main.py [district_number]")
+        exit(1)
+    
+    # check if map exists
+    if not path.exists(f"data/district_{argv[1]}"):
+        print("Usage: python3 main.py [district_number]")
+        exit(1)
+
+    number = argv[1]
+    
+    file_batteries = f"data/district_{number}/district-{number}_batteries.csv"
+    file_houses = f"data/district_{number}/district-{number}_houses.csv"
     grid1 = grid.Grid(file_batteries, file_houses)
 
-    visualise.visualise(grid1)
+    visualise.visualise(grid1, argv[1])
