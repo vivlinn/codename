cd772d2f64081f6047b202cefaae79018cacd980
VincentEngelhard <engelhardvincent@gmail.com>
2021-06-09 12:58:45 +0200

    bypass other batteries

--- code/algorithms/random_cables.py
+++ code/algorithms/random_cables.py
@@ -8,16 +8,16 @@ def random_cables(grid):
     for house in grid.houses:
 
         while True:
-            battery = random.choice(grid.batteries)
-            if battery.remaining >= house.max_output:
+            battery_chosen = random.choice(grid.batteries)
+            if battery_chosen.remaining >= house.max_output:
 
-                # update remaining battery capacity
-                battery.remaining = battery.remaining - house.max_output
+                # update remaining battery_chosen capacity
+                battery_chosen.remaining = battery_chosen.remaining - house.max_output
                 break
 
-        house.route = Route(battery, house.position_x, house.position_y)
+        house.route = Route(battery_chosen, house.position_x, house.position_y)
 
-        while battery.position_x != house.route.list_x[-1] or battery.position_y != house.route.list_y[-1]:
+        while battery_chosen.position_x != house.route.list_x[-1] or battery_chosen.position_y != house.route.list_y[-1]:
 
             direction = random.choice(['x', 'y'])
 
@@ -28,7 +28,7 @@ def random_cables(grid):
                 
                 # check if still inside grid
                 if xtest >= 0 and xtest <= grid.grid_width:
-
+                    
                     # check if route has started
                     if len(house.route.list_x) > 1:
                         # set previous cordinate as previous
@@ -38,12 +38,18 @@ def random_cables(grid):
                         if previous == xtest:
                             # choose new coordinate
                             continue
-                    
-                        # append new coordinate to route list
-                        house.route.list_x.append(xtest)
 
-                        # append unchanged y coordinate to route list
-                        house.route.list_y.append(ytest)
+                    for battery in grid.batteries:
+                        
+                        # bypass other batteries 
+                        if (xtest != battery.position_x or ytest != battery.position_y) and battery is battery_chosen:
+                    
+                            # append new coordinate to route list
+                            house.route.list_x.append(xtest)
+
+                            # append unchanged y coordinate to route list
+                            house.route.list_y.append(ytest)
+                            break
 
             else:
                 direction_y = random.choice([-1, 1])
@@ -51,15 +57,35 @@ def random_cables(grid):
                 xtest = house.route.list_x[-1]
 
                 if ytest>= 0 and ytest <= grid.grid_height:
+
+                    # check if route has started
                     if len(house.route.list_y) > 1:
+                        # set previous cordinate as previous
                         previous = house.route.list_y[-2]
-                    
+
+                        # check if new coordinate was not the previous coordinate
                         if previous == ytest:
+                            # choose new coordinate
                             continue
 
-                    house.route.list_y.append(ytest)
-            
-                    house.route.list_x.append(xtest)
+                    for battery in grid.batteries:
+
+                        # bypass other batteries 
+                        if ytest != battery.position_y or xtest != battery.position_x or battery is battery_chosen:
+                            
+                            # append new coordinate to route list
+                            house.route.list_y.append(ytest)
+
+                            # append unchanged y coordinate to route list
+                            house.route.list_x.append(xtest)
+                            break
+
+
+
+
+
+
+
 
         print(house.route.list_x) 
         print(house.route.list_y)  
