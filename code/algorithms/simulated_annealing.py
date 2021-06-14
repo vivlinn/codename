54ff724e7044182040e988c7762ece9d89546962
VincentEngelhard <engelhardvincent@gmail.com>
2021-06-14 12:44:56 +0200

    added mutate function

--- code/algorithms/simulated_annealing.py
+++ code/algorithms/simulated_annealing.py
@@ -2,6 +2,7 @@ from main import ITERATIONS
 from . import greedy, random
 from code.classes.route import Route
 import copy
+import random
 
 # Herhaal:
     # Kies een random start state
@@ -29,7 +30,7 @@ class simulated_annealing():
 
         # HERHAAL N ITERATIES
         for i in range(self.iterations):
-            mutate()
+            mutate(start_state)
             check()
 
             # change temperature
@@ -45,11 +46,68 @@ class simulated_annealing():
             succes = random.assign_battery(grid_copy)
 
             if succes == True:
-                state = greedy.create_cables(grid_copy)
+                start_state = greedy.create_cables(grid_copy)
                 break
             else:
                 print(succes)
 
+        return start_state
 
-    def mutate(self):
-        pass
+    def mutate(self, start_state):
+        # itereren over batterijen
+            # haal vijf random huizen eruit
+            # stop deze in de lijst
+        
+        # itereer over lengte van de lijst
+            # kies random huis uit lijst
+                # itereer over batterijen
+                    # koppel dit huis aan de batterij
+                        # check of het is gelukt
+                            # return false
+        # return true
+
+        old_state = copy.deepcopy(start_state)
+
+        houses_left = []
+        for battery in old_state.batteries:
+            # shuffle connected houses in random order
+            random.shuffle(battery.connected_houses)
+
+            for i in range(5):
+                # remove house from battery
+                house = battery.connected_houses.pop()
+                # update remaining capacity
+                battery.remaining += house.max_output
+                # add house to houses_left
+                houses_left.append(house)
+
+
+        random.shuffle(houses_left)
+        for house in houses_left:
+            # loop till a battery is assigned to a house
+            while True:
+
+                succes = False
+                for battery in old_state.batteries:
+                    if house.max_output <= battery.remaining:
+                        succes = True
+                        break
+                
+
+                # assign random battery to the house
+                battery_chosen = random.choice(old_state.batteries)
+
+                # check if battery has capacity for the house
+                if battery_chosen.remaining >= house.max_output:
+
+                    # update remaining battery_chosen capacity
+                    battery_chosen.remaining = battery_chosen.remaining - house.max_output
+                    break
+                
+                if succes == False:
+                    return succes
+
+            # add route object to the house
+            battery_chosen.connected_houses.append(house)
+            house.route = Route(battery_chosen, house.position_x, house.position_y)
+
