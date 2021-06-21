1f6cdad7b71a13e0de7ed5003b57119431d04d9d
vivlinn <pamelasneekes@gmail.com>
2021-06-21 09:06:03 +0200

    add comments to costs and longrun.py

--- code/visualisation/longrun.py
+++ code/visualisation/longrun.py
@@ -1,21 +1,24 @@
 import json
 
 def write_to_file(total_costs, state):
+    """
+    Saves results from longrunning the algorithms and saves in a file.
+    """
 
     _, y = state.plot()
     
     data = json.load(open('output/longrun_hill.json'))
 
-    # convert data to list if not
+    # Convert data to list if not
     if type(data) is dict:
         data = [data]
 
     dictionary = {"costs": total_costs, "iterations": len(y)}
 
-    # append new item to data lit
+    # Append new item to data lit
     data.append(dictionary)
 
-    # write list to file
+    # Write list to file
     with open('output/longrun_hill.json', 'w') as outfile:
         json.dump(data, outfile)
 
